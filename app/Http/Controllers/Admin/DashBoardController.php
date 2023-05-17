388580b4990a720907a0e9d31d5afc167198cb1a
Herenyon <rikitur17@gmail.com>
2023-05-17 16:43:54 +0200

    correzione rotta per dashboard.blade e modifiche stilistiche nella setssa

--- app/Http/Controllers/Admin/DashBoardController.php
+++ app/Http/Controllers/Admin/DashBoardController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Portf;
 use Illuminate\Http\Request;
 
 class DashBoardController extends Controller
 {
     public function index()
     {
-        return view('admin.portfo.index');
+        $portf = Portf::all();
+        return view('admin.dashboard', compact('portf'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header d-flex justify-content-between">
+                   <div>Welcome Back, {{ Auth::user()->name }}</div> 
+                   <div><a href="{{route('admin.portf.index')}}" class="btn btn-small btn-secondary mx-1">More Repo</a></div>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +16,30 @@
                         {{ session('status') }}
                     </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    <table class="table table-dark table-striped">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Repo Name</th>
+                            <th scope="col">Author</th>
+                            <th scope="col">Date of Creation</th>
+                            
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @for ($i = 0; $i < 3; $i++) 
+                                <tr>
+                                    <th scope="row">{{ $portf[$i]->id }}</th>
+                                    <td>{{ $portf[$i]->repo_title }}</td>
+                                    <td>{{ $portf[$i]->author }}</td>
+                                    <td>{{ $portf[$i]->date_of_start }}</td>    
+                                </tr>
+                            @endfor
+                                
+                            
+                        </tbody>
+                    </table>
+                    
                 </div>
             </div>
         </div>
